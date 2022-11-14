5e36f0a245224e3f4946feb9e32a8db5e5d558ae
Kevin Le <kevinle6231@gmail.com>
2022-11-14 13:42:44 -0500

    updated merge with main

--- src/main/java/DataBase.java
+++ src/main/java/DataBase.java
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-public class DataBase {
 
-=======
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -13,81 +10,88 @@ import java.io.IOException;
 import java.util.Objects;
 import java.util.Scanner;
 
-public class DataBase {
-    public static String getUserFilePath() {
-        return "src/main/database/users.txt";
-    }
-
-    /** Creates a new file at the specified directory
-     * @param fileDirectory File directory to create a new file at
-     * */
-    public static void createFile(String fileDirectory) {
-        try {
-            File file = new File(fileDirectory);
-
-            file.createNewFile();
-        } catch (IOException e) {
-            System.out.println("An error occurred creating file.");
-            e.printStackTrace();
-        }
-    }
-
-    /** Adds a new user to the database in JSON format
-     * @param user user to add to the database
-     * @param password password passed from user to store in the database
-     * */
-    public static boolean addUser(User user, String password) {
-        File file = new File(DataBase.getUserFilePath());
-
-        // If the file directory doesn't exist, create a new file
-        if (!file.isFile()) {
-           createFile(DataBase.getUserFilePath());
+    public class DataBase {
+        public static String getUserFilePath() {
+            return "src/main/database/users.txt";
         }
 
-        try {
-            FileWriter fileWriter = new FileWriter(DataBase.getUserFilePath(), true);
-            JSONObject userObject = new JSONObject();
-            userObject.put("user", user.getName());
-            userObject.put("password", password);
-            userObject.put("currency", user.getCurrency());
-            fileWriter.write(userObject.toJSONString() + '\n');
-            fileWriter.close();
-            return true;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+        /**
+         * Creates a new file at the specified directory
+         *
+         * @param fileDirectory File directory to create a new file at
+         */
+        public static void createFile(String fileDirectory) {
+            try {
+                File file = new File(fileDirectory);
 
-    /** Retrieves and returns a User object based on name
-     * @param name Unique name of the user
-     * */
-    public static User getUser(String name) {
-        try {
-            File myObj = new File(DataBase.getUserFilePath());
-            Scanner myReader = new Scanner(myObj);
-            // Find the first user with the correct name
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                JSONParser jsonParser = new JSONParser();
-                JSONObject parsedData = (JSONObject) jsonParser.parse(data);
-                if (Objects.equals(parsedData.get("user").toString(), name)) {
-                    String userName = (String) parsedData.get("user");
-                    String password = (String) parsedData.get("password");
-                    String currency = (String) parsedData.get("currency");
-                    return new User(userName, password, currency);
-                }
+                file.createNewFile();
+            } catch (IOException e) {
+                System.out.println("An error occurred creating file.");
+                e.printStackTrace();
             }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred file not found.");
-            e.printStackTrace();
-        } catch (ParseException e) {
-            System.out.println("An error occurred failed to parse user data.");
-            e.printStackTrace();
         }
-        // Return a default user if user doesn't exist
-        return new User("Default User", "Password");
+
+        /**
+         * Adds a new user to the database in JSON format
+         *
+         * @param user     user to add to the database
+         * @param password password passed from user to store in the database
+         */
+        public static boolean addUser(User user, String password) {
+            File file = new File(DataBase.getUserFilePath());
+
+            // If the file directory doesn't exist, create a new file
+            if (!file.isFile()) {
+                createFile(DataBase.getUserFilePath());
+            }
+
+            try {
+                FileWriter fileWriter = new FileWriter(DataBase.getUserFilePath(), true);
+                JSONObject userObject = new JSONObject();
+                userObject.put("user", user.getName());
+                userObject.put("password", password);
+                userObject.put("currency", user.getCurrency());
+                fileWriter.write(userObject.toJSONString() + '\n');
+                fileWriter.close();
+                return true;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        /**
+         * Retrieves and returns a User object based on name
+         *
+         * @param name Unique name of the user
+         */
+        public static User getUser(String name) {
+            try {
+                File myObj = new File(DataBase.getUserFilePath());
+                Scanner myReader = new Scanner(myObj);
+                // Find the first user with the correct name
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    JSONParser jsonParser = new JSONParser();
+                    JSONObject parsedData = (JSONObject) jsonParser.parse(data);
+                    if (Objects.equals(parsedData.get("user").toString(), name)) {
+                        String userName = (String) parsedData.get("user");
+                        String password = (String) parsedData.get("password");
+                        String currency = (String) parsedData.get("currency");
+                        return new User(userName, password, currency);
+                    }
+                }
+                myReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred file not found.");
+                e.printStackTrace();
+            } catch (ParseException e) {
+                System.out.println("An error occurred failed to parse user data.");
+                e.printStackTrace();
+            }
+            // Return a default user if user doesn't exist
+            return new User("Default User", "Password");
+        }
+
     }
->>>>>>> origin/add-user-read-write
-}
+
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,15 +1,24 @@
+import java.io.IOException;
+
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         Item keyboard = new Item("Keyboard", 300.1, 250.0, "www.amazon.com/keyboard",
-                "Description from amazon (or you write your own)", new String[] {"computer accesssories", "Tech", "Mechanical"});
+                "Description from amazon (or you write your own)", new String[] {"computer accesssories", "Tech", "Mechanical"},0, 0 );
         Item monitor = new Item("Monitor", 120.99, 85.00, "www.amazon.com/monitor",
-                "second monitor dab", new String[] {"computer accesssories", "Tech", "IPS"});
+                "second monitor dab", new String[] {"computer accesssories", "Tech", "IPS"},0, 0 );
         Item plushie = new Item("Plushie", 40.99, 30.00, "www.amazon.com/plushie",
-                "Description from amazon (or you write your own)", new String[] {"toys"});
-
+                "Description from amazon (or you write your own)", new String[] {"toys"}, 0, 0 );
+        Item priceUpdateTestItem = new Item("AmazonBasics Wired Keyboard", 20.00, 15.00, "https://www.amazon.ca/AmazonBasics-KU-0833-Wired-Keyboard/dp/B005EOWBHC/ref=sr_1_6?crid=LXQRVB06NTVV&keywords=keyboard&qid=1668040664&qu=eyJxc2MiOiI3LjM4IiwicXNhIjoiNi42NSIsInFzcCI6IjUuOTMifQ%3D%3D&sprefix=keyboard%2Caps%2C90&sr=8-6&th=1",
+                "Low-profile Keys Provide a Quiet, Comfortable Typing Experience\n" +
+                        "Hotkeys Enable Easy Access for Media, My Computer, Mute, Volume down, Volume up, and Calculator; 4 Function Keys Control Previous Track, Stop, Play/pause, next Track on Your Media Player\n" +
+                        "Simple Wired USB Connection; Works with Windows 2000, XP, Vista, 7, 8, and 10\n" +
+                        "Backed by One-year Amazon Basics Warranty\n" +
+                        "Ships in Certified Frustration-free Packaging", new String[] {"computer accesssories", "Tech", "office"}, 0, 0 );
         Wishlist christmasWishlist = new Wishlist("Christmas Wishlist");
-
+        priceUpdateTestItem.displayItemInConsole(0);
+        priceUpdateTestItem.updatePrice();
+        priceUpdateTestItem.displayItemInConsole(0);
 
         System.out.println(christmasWishlist.addItem(keyboard));
         System.out.println(christmasWishlist.addItem(monitor));
@@ -17,5 +26,11 @@ public class main {
         christmasWishlist.displayList();
         christmasWishlist.filterWishlists(new String[] {"Tech"});
         christmasWishlist.displayList();
+
+//        SearchitemsApi apiSearcher = new SearchitemsApi();
+//
+//        ArrayList itemList = apiSearcher.searchToList("mechanical keyboard", "CA");
+
+
     }
 }
