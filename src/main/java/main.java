c7c575a57fe5d7ab724be072c3d3887680e8d7f9
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2022-11-14 19:29:56 -0500

    Revert "Item Search Api Implementation"

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,15 +1,14 @@
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Objects;
 
 public class main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
 
         Item keyboard = new Item("Keyboard", 300.1, 250.0, "www.amazon.com/keyboard",
-                "Description from amazon (or you write your own)", new String[] {"computer accesssories", "Tech", "Mechanical"}, 0, 0);
+                "Description from amazon (or you write your own)", new String[] {"computer accesssories", "Tech", "Mechanical"});
         Item monitor = new Item("Monitor", 120.99, 85.00, "www.amazon.com/monitor",
-                "second monitor dab", new String[] {"computer accesssories", "Tech", "IPS"}, 0, 0);
+                "second monitor dab", new String[] {"computer accesssories", "Tech", "IPS"});
         Item plushie = new Item("Plushie", 40.99, 30.00, "www.amazon.com/plushie",
-                "Description from amazon (or you write your own)", new String[] {"toys"}, 0, 0);
+                "Description from amazon (or you write your own)", new String[] {"toys"});
 
         Wishlist christmasWishlist = new Wishlist("Christmas Wishlist");
 
@@ -21,28 +20,8 @@ public class main {
         christmasWishlist.filterWishlists(new String[] {"Tech"});
         christmasWishlist.displayList();
 
-        Item priceUpdateTestItem = new Item("AmazonBasics Wired Keyboard", 20.00, 15.00, "https://www.amazon.ca/AmazonBasics-KU-0833-Wired-Keyboard/dp/B005EOWBHC/ref=sr_1_6?crid=LXQRVB06NTVV&keywords=keyboard&qid=1668040664&qu=eyJxc2MiOiI3LjM4IiwicXNhIjoiNi42NSIsInFzcCI6IjUuOTMifQ%3D%3D&sprefix=keyboard%2Caps%2C90&sr=8-6&th=1",
-                "Low-profile Keys Provide a Quiet, Comfortable Typing Experience\n" +
-                        "Hotkeys Enable Easy Access for Media, My Computer, Mute, Volume down, Volume up, and Calculator; 4 Function Keys Control Previous Track, Stop, Play/pause, next Track on Your Media Player\n" +
-                        "Simple Wired USB Connection; Works with Windows 2000, XP, Vista, 7, 8, and 10\n" +
-                        "Backed by One-year Amazon Basics Warranty\n" +
-                        "Ships in Certified Frustration-free Packaging", new String[] {"computer accesssories", "Tech", "office"}, 0, 0 );
-
-        priceUpdateTestItem.displayItemInConsole(0);
-        priceUpdateTestItem.updatePrice();
-        priceUpdateTestItem.displayItemInConsole(0);
-
-        SearchitemsApi apiSearcher = new SearchitemsApi();
-
-        //
-
-        ArrayList<Item> itemList = apiSearcher.searchToList("mechanical keyboard", "CA");
-
-//        for (int i = 0; i < 3; i++){
-//            itemList.get(i).displayItemInConsole(i + 1);
-//        }
-
-
-
+        User user = new User("Herman1", "Password");
+        // user.writeUserToDatabase();
+        System.out.println(DataBase.getUser("Herman1").getName());
     }
 }
