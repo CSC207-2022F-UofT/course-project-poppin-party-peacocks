ec4e326a8e98a191c26e8b4e37ca42f7a3d0034c
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2022-11-18 20:52:06 -0500

    attempt to fix merging issue with main

--- src/main/java/ListOfWishlists.java
+++ src/main/java/ListOfWishlists.java
@@ -4,8 +4,8 @@ public class ListOfWishlists {
 
     ArrayList<Wishlist> listWishlist;
     
-    public ListOfWishlists(){
-        this.listWishlist = new ArrayList<Wishlist>();
+    public ListOfWishlists(ArrayList<Wishlist> wishlists){
+        this.listWishlist = wishlists;
     }
 
     public void addWishlist(Wishlist wishlist){
