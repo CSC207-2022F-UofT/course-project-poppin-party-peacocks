7a15202576c397a7b07ad0e26949abff8934bb83
AlexChenl3 <95944203+AlexChenl3@users.noreply.github.com>
2022-11-21 13:15:00 -0500

    testing ItemNameComparatorTest

--- src/test/java/ItemNameComparatorTest.java
+++ src/test/java/ItemNameComparatorTest.java
@@ -1,2 +1,29 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class ItemNameComparatorTest {
+    Item myFavDrink = new Item("Lime Bubbly", 5.47, 5.00, "www.shoppers.com/bubbly",
+            "my favorite drink, bubbly", new String[]{"Drink"}, 69, 4.19,"www.shoppersimage.com/bubbly");
+    Item animeFigure = new Item("Starlight Anya Forger", 100, 85.00, "www.amazon.com/AnyaPeanuts", "new Anya figure", new String[]{"Figure"}, 150, 4.8,"www.amazonimage.com/AnyaPeanuts" );
+    Item plushie = new Item("Whale Plushie", 40.99, 30.00, "www.amazon.com/WhalePlushie",
+            "Giant Whale Plushie", new String[]{"toys"}, 1050, 4.3, "www.amazonimage.com/OhWhale");
+
+    Wishlist christmasWishlist = new Wishlist("Christmas Wishlist");
+
+    @Test
+    public void ItemNameComparatorTest() {
+        christmasWishlist.addItem(myFavDrink);
+        christmasWishlist.addItem(animeFigure);
+        christmasWishlist.addItem(plushie);
+
+        Wishlist testingWishlist = new Wishlist("Testing Wishlist");
+        testingWishlist.addItem(myFavDrink);
+        testingWishlist.addItem(animeFigure);
+        testingWishlist.addItem(plushie);
+
+        christmasWishlist.sortWishlistByName("ascending");
+        testingWishlist.sortWishlistByName("ascending");
+
+        Assertions.assertEquals(testingWishlist, christmasWishlist);
+    }
 }
--- src/test/java/WishlistAppTest.java
+++ src/test/java/WishlistAppTest.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import tutorial.HelloWorld;
 
 import java.io.IOException;
 import java.util.ArrayList;
