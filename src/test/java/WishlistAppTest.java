e77da78a7f3df435665fb7b71cd3a646a450e17d
PyroPoro <rickywen12345@gmail.com>
2022-11-21 01:04:24 -0500

    commented out the api tests since they were using up limited api calls

--- src/test/java/WishlistAppTest.java
+++ src/test/java/WishlistAppTest.java
@@ -6,30 +6,30 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class WishlistAppTest {
-    /**
-     * Testing search item feature and if all items in the return list are of Object Entities.Item
-     */
-    @Test
-    public void searchItemTestItemInstance() throws IOException, InterruptedException {
-        SearchitemsApi apiSearcher = new SearchitemsApi();
-
-        ArrayList itemSearchList = apiSearcher.searchToList("mechanical keyboard", "CA");
-        for (Object item : itemSearchList) {
-            Assertions.assertEquals(true, item instanceof Item);
-        }
-    }
-
-    /**
-     * Testing search item feature and if 10 items were correct added to the list
-     */
-    @Test
-    public void searchItemTestListLengthReturn() throws IOException, InterruptedException {
-        SearchitemsApi apiSearcher = new SearchitemsApi();
-
-        ArrayList itemSearchList = apiSearcher.searchToList("mechanical keyboard", "CA");
-
-        Assertions.assertEquals(10, itemSearchList.size());
-    }
+//    /**
+//     * Testing search item feature and if all items in the return list are of Object Entities.Item
+//     */
+//    @Test
+//    public void searchItemTestItemInstance() throws IOException, InterruptedException {
+//        SearchitemsApi apiSearcher = new SearchitemsApi();
+//
+//        ArrayList itemSearchList = apiSearcher.searchToList("mechanical keyboard", "CA");
+//        for (Object item : itemSearchList) {
+//            Assertions.assertEquals(true, item instanceof Item);
+//        }
+//    }
+//
+//    /**
+//     * Testing search item feature and if 10 items were correct added to the list
+//     */
+//    @Test
+//    public void searchItemTestListLengthReturn() throws IOException, InterruptedException {
+//        SearchitemsApi apiSearcher = new SearchitemsApi();
+//
+//        ArrayList itemSearchList = apiSearcher.searchToList("mechanical keyboard", "CA");
+//
+//        Assertions.assertEquals(10, itemSearchList.size());
+//    }
 
     /**
      * Testing updatePrice feature and if price has been updated (different price than initial price)
