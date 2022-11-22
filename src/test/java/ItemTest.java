6517561319e97be49b83024c74cec37a4dba23e1
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2022-11-21 21:37:55 -0500

    Update ItemTest.java

--- src/test/java/ItemTest.java
+++ src/test/java/ItemTest.java
@@ -1,10 +1,10 @@
+import Entities.Item;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
-import Entities.*;
-import DataBase.DataBase;
+
 public class ItemTest {
+
     /**
      * Testing updatePrice feature and if price has been updated (different price than initial price)
      */
@@ -16,14 +16,18 @@ public class ItemTest {
                         "Simple Wired USB Connection; Works with Windows 2000, XP, Vista, 7, 8, and 10\n" +
                         "Backed by One-year Amazon Basics Warranty\n" +
                         "Ships in Certified Frustration-free Packaging", new String[]{"computer accesssories", "Tech", "office"}, 0, 0, "www.imageurl.com");
+
         double initialPrice = priceUpdateTestItem.getItemPrice();
+
         priceUpdateTestItem.updatePrice();
+
         double newPrice = priceUpdateTestItem.getItemPrice();
+
         Assertions.assertEquals(true, initialPrice != newPrice);
     }
 
     /**
-     * Testing setter and getter for Object Item name variable
+     * Testing setter and getter for Object Entities.Item name variable
      */
     @Test
     public void itemClassTestSetGetName() {
@@ -33,17 +37,15 @@ public class ItemTest {
                         "Simple Wired USB Connection; Works with Windows 2000, XP, Vista, 7, 8, and 10\n" +
                         "Backed by One-year Amazon Basics Warranty\n" +
                         "Ships in Certified Frustration-free Packaging", new String[]{"computer accesssories", "Tech", "office"}, 0, 0, "imageurl");
-
         // Test 1:  Set and Get Name
         TestItem.setName("AmazonBasics Wired Office Keyboard");
-        Assertions.assertEquals(true, "AmazonBasics Wired Office Keyboard" == TestItem.getItemName());
         Assertions.assertEquals(true, "AmazonBasics Wired Office Keyboard".equals(TestItem.getItemName()));
 
 
     }
 
     /**
-     * Testing getter for Object Item price variable
+     * Testing getter for Object Entities.Item price variable
      */
     @Test
     public void itemClassTestGetPrice() throws IOException {
@@ -58,7 +60,7 @@ public class ItemTest {
     }
 
     /**
-     * Testing setter and getter for Object Item desired price
+     * Testing setter and getter for Object Entities.Item desired price
      */
     @Test
     public void itemClassTestSetGetDesiredPrice() {
@@ -68,12 +70,13 @@ public class ItemTest {
                         "Simple Wired USB Connection; Works with Windows 2000, XP, Vista, 7, 8, and 10\n" +
                         "Backed by One-year Amazon Basics Warranty\n" +
                         "Ships in Certified Frustration-free Packaging", new String[]{"computer accesssories", "Tech", "office"}, 0, 0, "imageurl");
+
         TestItem.setDesiredPrice(17.00);
         Assertions.assertEquals(true, 17.00 == TestItem.getItemDesiredPrice());
     }
 
     /**
-     * Testing getter for Object Item url variable
+     * Testing getter for Object Entities.Item url variable
      */
     @Test
     public void itemClassTestSetGetUrl() {
@@ -84,11 +87,12 @@ public class ItemTest {
                         "Backed by One-year Amazon Basics Warranty\n" +
                         "Ships in Certified Frustration-free Packaging", new String[]{"computer accesssories", "Tech", "office"}, 0, 0, "imageurl");
         String testUrl = "https://www.amazon.ca/AmazonBasics-KU-0833-Wired-Keyboard/dp/B005EOWBHC/ref=sr_1_6?crid=LXQRVB06NTVV&keywords=keyboard&qid=1668040664&qu=eyJxc2MiOiI3LjM4IiwicXNhIjoiNi42NSIsInFzcCI6IjUuOTMifQ%3D%3D&sprefix=keyboard%2Caps%2C90&sr=8-6&th=1";
-        Assertions.assertEquals(true, testUrl == TestItem.getItemURL());
+
+        Assertions.assertEquals(true, testUrl.equals(TestItem.getItemURL()));
     }
 
     /**
-     * Testing getter for Object Item imageUrl variable
+     * Testing getter for Object Entities.Item imageUrl variable
      */
     @Test
     public void itemClassTestSetGetImageUrl() throws IOException {
@@ -98,13 +102,13 @@ public class ItemTest {
                         "Simple Wired USB Connection; Works with Windows 2000, XP, Vista, 7, 8, and 10\n" +
                         "Backed by One-year Amazon Basics Warranty\n" +
                         "Ships in Certified Frustration-free Packaging", new String[]{"computer accesssories", "Tech", "office"}, 0, 0, "imageurl");
-
         String testImageUrl = "imageurl";
-        Assertions.assertEquals(true, testImageUrl == TestItem.getItemImageURL());
+
+        Assertions.assertEquals(true, testImageUrl.equals(TestItem.getItemImageURL()));
     }
 
     /**
-     * Testing setter and getter for Object Item item description variable
+     * Testing setter and getter for Object Entities.Item item description variable
      */
     @Test
     public void itemClassTestSetGetItemDescription() throws IOException {
@@ -115,12 +119,13 @@ public class ItemTest {
                         "Backed by One-year Amazon Basics Warranty\n" +
                         "Ships in Certified Frustration-free Packaging", new String[]{"computer accesssories", "Tech", "office"}, 0, 0, "imageurl");
         String newDescription = "This is a new set item description for AmazonBasics Wired Office Keyboard";
+
         TestItem.setItemDescription(newDescription);
-        Assertions.assertEquals(true, newDescription == TestItem.getItemDescription());
+        Assertions.assertEquals(true, newDescription.equals(TestItem.getItemDescription()));
     }
 
     /**
-     * Testing setter and getter for Object Item review count variable
+     * Testing setter and getter for Object Entities.Item review count variable
      */
     @Test
     public void itemClassTestSetGetReviewCount() throws IOException {
@@ -131,13 +136,12 @@ public class ItemTest {
                         "Backed by One-year Amazon Basics Warranty\n" +
                         "Ships in Certified Frustration-free Packaging", new String[]{"computer accesssories", "Tech", "office"}, 0, 0, "imageurl");
 
-
         TestItem.setReviewCount(30);
         Assertions.assertEquals(true, 30 == TestItem.getReviewCount());
     }
 
     /**
-     * Testing setter and getter for Object Item review star variable
+     * Testing setter and getter for Object Entities.Item review star variable
      */
     @Test
     public void itemClassTestSetGetReviewStar() throws IOException {
@@ -147,57 +151,8 @@ public class ItemTest {
                         "Simple Wired USB Connection; Works with Windows 2000, XP, Vista, 7, 8, and 10\n" +
                         "Backed by One-year Amazon Basics Warranty\n" +
                         "Ships in Certified Frustration-free Packaging", new String[]{"computer accesssories", "Tech", "office"}, 0, 0, "imageurl");
+
         TestItem.setReviewStars(4.2);
         Assertions.assertEquals(true, 4.2 == TestItem.getReviewStars());
-    }
-
-    @Test
-    public void itemGetsCurrency() throws IOException {
-        Item testItem = new Item("AmazonBasics Wired Keyboard", 20.00, 15.00, "https://www.amazon.ca/AmazonBasics-KU-0833-Wired-Keyboard/dp/B005EOWBHC/ref=sr_1_6?crid=LXQRVB06NTVV&keywords=keyboard&qid=1668040664&qu=eyJxc2MiOiI3LjM4IiwicXNhIjoiNi42NSIsInFzcCI6IjUuOTMifQ%3D%3D&sprefix=keyboard%2Caps%2C90&sr=8-6&th=1",
-                "Low-profile Keys Provide a Quiet, Comfortable Typing Experience\n" +
-                        "Hotkeys Enable Easy Access for Media, My Computer, Mute, Volume down, Volume up, and Calculator; 4 Function Keys Control Previous Track, Stop, Play/pause, next Track on Your Media Player\n" +
-                        "Simple Wired USB Connection; Works with Windows 2000, XP, Vista, 7, 8, and 10\n" +
-                        "Backed by One-year Amazon Basics Warranty\n" +
-                        "Ships in Certified Frustration-free Packaging", new String[]{"computer accesssories", "Tech", "office"}, 0, 0, "imageurl", "CAD");
-       Assertions.assertEquals(testItem.getItemCurrency(), "CAD");
-    }
-
-    @Test
-    public void itemUpdatesCurrencyCAD() throws IOException {
-        Item testItem = new Item("AmazonBasics Wired Keyboard", 1.0, 15.00, "https://www.amazon.ca/AmazonBasics-KU-0833-Wired-Keyboard/dp/B005EOWBHC/ref=sr_1_6?crid=LXQRVB06NTVV&keywords=keyboard&qid=1668040664&qu=eyJxc2MiOiI3LjM4IiwicXNhIjoiNi42NSIsInFzcCI6IjUuOTMifQ%3D%3D&sprefix=keyboard%2Caps%2C90&sr=8-6&th=1",
-                "Low-profile Keys Provide a Quiet, Comfortable Typing Experience\n" +
-                        "Hotkeys Enable Easy Access for Media, My Computer, Mute, Volume down, Volume up, and Calculator; 4 Function Keys Control Previous Track, Stop, Play/pause, next Track on Your Media Player\n" +
-                        "Simple Wired USB Connection; Works with Windows 2000, XP, Vista, 7, 8, and 10\n" +
-                        "Backed by One-year Amazon Basics Warranty\n" +
-                        "Ships in Certified Frustration-free Packaging", new String[]{"computer accesssories", "Tech", "office"}, 0, 0, "imageurl", "CAD");
-        DataBase.currentUser = new User("A", "B", "USD");
-        testItem.updateCurrency();
-        Assertions.assertEquals(testItem.getItemPrice(), 0.76);
-    }
-
-    @Test
-    public void itemUpdatesCurrencyUSD() throws IOException {
-        Item testItem = new Item("AmazonBasics Wired Keyboard", 1.0, 15.00, "https://www.amazon.ca/AmazonBasics-KU-0833-Wired-Keyboard/dp/B005EOWBHC/ref=sr_1_6?crid=LXQRVB06NTVV&keywords=keyboard&qid=1668040664&qu=eyJxc2MiOiI3LjM4IiwicXNhIjoiNi42NSIsInFzcCI6IjUuOTMifQ%3D%3D&sprefix=keyboard%2Caps%2C90&sr=8-6&th=1",
-                "Low-profile Keys Provide a Quiet, Comfortable Typing Experience\n" +
-                        "Hotkeys Enable Easy Access for Media, My Computer, Mute, Volume down, Volume up, and Calculator; 4 Function Keys Control Previous Track, Stop, Play/pause, next Track on Your Media Player\n" +
-                        "Simple Wired USB Connection; Works with Windows 2000, XP, Vista, 7, 8, and 10\n" +
-                        "Backed by One-year Amazon Basics Warranty\n" +
-                        "Ships in Certified Frustration-free Packaging", new String[]{"computer accesssories", "Tech", "office"}, 0, 0, "imageurl", "USD");
-        DataBase.currentUser = new User("A", "B", "CAD");
-        testItem.updateCurrency();
-        Assertions.assertEquals(testItem.getItemPrice(), 1.34);
-    }
-
-    @Test
-    public void itemUpdatesCurrencyYUAN() throws IOException {
-        Item testItem = new Item("AmazonBasics Wired Keyboard", 1.0, 15.00, "https://www.amazon.ca/AmazonBasics-KU-0833-Wired-Keyboard/dp/B005EOWBHC/ref=sr_1_6?crid=LXQRVB06NTVV&keywords=keyboard&qid=1668040664&qu=eyJxc2MiOiI3LjM4IiwicXNhIjoiNi42NSIsInFzcCI6IjUuOTMifQ%3D%3D&sprefix=keyboard%2Caps%2C90&sr=8-6&th=1",
-                "Low-profile Keys Provide a Quiet, Comfortable Typing Experience\n" +
-                        "Hotkeys Enable Easy Access for Media, My Computer, Mute, Volume down, Volume up, and Calculator; 4 Function Keys Control Previous Track, Stop, Play/pause, next Track on Your Media Player\n" +
-                        "Simple Wired USB Connection; Works with Windows 2000, XP, Vista, 7, 8, and 10\n" +
-                        "Backed by One-year Amazon Basics Warranty\n" +
-                        "Ships in Certified Frustration-free Packaging", new String[]{"computer accesssories", "Tech", "office"}, 0, 0, "imageurl", "YUAN");
-        DataBase.currentUser = new User("A", "B", "USD");
-        testItem.updateCurrency();
-        Assertions.assertEquals(testItem.getItemPrice(), 0.14);
     }
 }
