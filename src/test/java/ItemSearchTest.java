1f0fd5c6bcb5cd88e8015614fa590ea9e626e30b
Kevin Le <kevinle6231@gmail.com>
2022-11-21 21:08:15 -0500

    added testing for item class object and the item search feature

---
+++ src/test/java/ItemSearchTest.java
@@ -0,0 +1,78 @@
+import Entities.Item;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ItemSearchTest {
+    /**
+     * Testing search item by keyword feature and if all items in the return list are of Object Entities.Item
+     */
+    @Test
+    public void searchItemTestItemInstance() throws IOException, InterruptedException {
+        ItemSearcher itemSearcher = new ItemSearcher();
+
+        ArrayList itemSearchList = itemSearcher.searchItemKeywords("mechanical keyboard");
+        for (Object item : itemSearchList) {
+            Assertions.assertEquals(true, item instanceof Item);
+        }
+    }
+
+    /**
+     * Testing search item feature and if 10 items were correct added to the list
+     */
+    @Test
+    public void searchItemTestListLengthReturn() throws IOException, InterruptedException {
+        ItemSearcher itemSearcher = new ItemSearcher();
+
+        ArrayList itemSearchList = itemSearcher.searchItemKeywords("mechanical keyboard");
+
+        Assertions.assertEquals(10, itemSearchList.size());
+    }
+
+    /**
+     * Testing search item by url feature and if it is scraping the correct value of price
+     */
+    @Test
+    public void searchItemTestKittenPlushPrice() throws IOException, InterruptedException {
+        ItemSearcher itemSearcher = new ItemSearcher();
+
+        String kittenplush = "https://www.amazon.ca/Kitten-Plush-Stuffed-Animal-Pillow/dp/B088BWPFYZ/ref=sr_1_5?crid=16S7RSE5N25AA&keywords=uwu+cat&qid=1669082008&qu=eyJxc2MiOiIyLjI1IiwicXNhIjoiMC4wMCIsInFzcCI6IjAuMDAifQ%3D%3D&sprefix=uwu+cat%2Caps%2C69&sr=8-5";
+        double expectedPrice = 19.20;
+        Item kittenplushitem = itemSearcher.searchItemUrl(kittenplush);
+
+        Assertions.assertEquals(kittenplushitem.getItemPrice(), expectedPrice);
+
+    }
+
+    /**
+     * Testing search item by url feature and if it is scraping the correct value of name
+     */
+    @Test
+    public void searchItemTestWorstPlayerToadName() throws IOException, InterruptedException {
+        ItemSearcher itemSearcher = new ItemSearcher();
+
+        String toadplush = "https://www.amazon.ca/uiuoutoy-Super-Mario-Bros-Mushroom/dp/B07CZGM7KV/ref=sr_1_1?crid=273IB3Y3TBWBZ&keywords=toad+mario+kart+plush&qid=1669082305&qu=eyJxc2MiOiIxLjQ1IiwicXNhIjoiMC4wMCIsInFzcCI6IjAuMDAifQ%3D%3D&s=books&sprefix=toad+mariokarplushie%2Cstripbooks%2C76&sr=1-1-catcorr";
+        String expectedName = "uiuoutoy Super Mario Bros. Red Toad Plush Mushroom 7''";
+        Item kittenplushitem = itemSearcher.searchItemUrl(toadplush);
+
+        Assertions.assertEquals(kittenplushitem.getItemName(), expectedName);
+
+    }
+    /**
+     * Testing search item by url feature and if it is scraping the correct value of number of ratings for the product
+     */
+    @Test
+    public void searchItemCarnivalCountRatingTest() throws IOException, InterruptedException {
+        ItemSearcher itemSearcher = new ItemSearcher();
+
+        String carnivalplush = "https://www.amazon.ca/BB-Funhouse-Stuffed-Carnival-3-14inch/dp/B09NRGTT1R/ref=sr_1_7?crid=1QCCDI9NB8WIQ&keywords=whale+plush&qid=1669082056&qu=eyJxc2MiOiI2LjAyIiwicXNhIjoiNS4xMiIsInFzcCI6IjQuNTQifQ%3D%3D&sprefix=whale+plushie%2Caps%2C95&sr=8-7";
+        Integer expectedCount = 7;
+        Item kittenplushitem = itemSearcher.searchItemUrl(carnivalplush);
+
+        Assertions.assertEquals(kittenplushitem.getReviewCount(), expectedCount);
+
+    }
+}
+
