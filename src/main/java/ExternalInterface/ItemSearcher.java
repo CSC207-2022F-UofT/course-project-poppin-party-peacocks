9bee0fdfc6234e7b7efc9ed094b350ea9aeea751
Kevin Le <kevinle6231@gmail.com>
2022-11-22 21:36:45 -0500

    fixed spacing

--- src/main/java/ExternalInterface/ItemSearcher.java
+++ src/main/java/ExternalInterface/ItemSearcher.java
@@ -14,7 +14,6 @@ import java.net.http.HttpResponse;
 import java.util.ArrayList;
 
 public class ItemSearcher {
-
     /**
      * Calls Amazon Api search tool to return json string of search results of specified keyword and marketplace
      *
@@ -22,7 +21,6 @@ public class ItemSearcher {
      * @param marketplace specified marketplace (ex: "CA" for Canada) to search in Amazon
      */
     private String apiSearch(String keywords, String marketplace) throws IOException, InterruptedException {
-
         String linkurl = keywordstext(keywords, marketplace);
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(linkurl))
@@ -32,8 +30,6 @@ public class ItemSearcher {
                 .build();
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
         return (response.body());
-
-
     }
 
     /**
@@ -42,8 +38,6 @@ public class ItemSearcher {
      * @param response string keyword to search in Amazon
      */
     private String cleanResponse(String response) {
-
-
         String modifiedResponse = response.replace("[{", "");
         modifiedResponse = modifiedResponse.replace("}]", "");
         modifiedResponse = modifiedResponse.replace('"', ' ');
@@ -61,13 +55,10 @@ public class ItemSearcher {
      * @param marketplace specified marketplace (ex: "CA" for Canada) to search in Amazon
      */
     public ArrayList searchToList(String keyword, String marketplace) throws IOException, InterruptedException {
-
         String response = apiSearch(keyword, marketplace);
         response = cleanResponse(response);
-
         ArrayList<Item> itemList = new ArrayList<Item>();
         String[] pairs = response.split(" , ");
-
         ArrayList<String> titleList = new ArrayList<String>();
         ArrayList<String> priceList = new ArrayList<String>();
         ArrayList<String> urlList = new ArrayList<String>();
@@ -77,11 +68,8 @@ public class ItemSearcher {
         for (int i = 0; i < pairs.length; i++) {
             String pair = pairs[i];
             String[] keyValue = pair.split(" :");
-
-
             if (keyValue[0].contains("title") && !keyValue[0].contains("subtitle")) {
                 titleList.add(keyValue[1]);
-
             }
             if (keyValue[0].contains("price")) {
                 priceList.add(keyValue[1].replace("$", ""));
@@ -89,15 +77,12 @@ public class ItemSearcher {
             if (keyValue[0].contains("detailPageURL")) {
                 urlList.add(keyValue[1]);
             }
-
             if (keyValue[0].contains("totalReviews")) {
                 reviewCountList.add(keyValue[1]);
             }
-
             if (keyValue[0].contains("rating")) {
                 reviewStarList.add(keyValue[1]);
             }
-
             if (keyValue[0].contains("imageUrl")) {
                 imageUrlList.add(keyValue[1]);
             }
@@ -106,7 +91,6 @@ public class ItemSearcher {
         for (int i = 0; i < titleList.size(); i++) {
             Item newItem = new Item(titleList.get(i), Double.parseDouble(priceList.get(i)), Double.parseDouble(priceList.get(i)), urlList.get(i), titleList.get(i), new String[]{keyword}, Integer.parseInt(reviewCountList.get(i).replace(" ", "")), Double.parseDouble(reviewStarList.get(i).replace(" ", "")), imageUrlList.get(i));
             itemList.add(newItem);
-
         }
         return itemList;
     }
@@ -118,7 +102,6 @@ public class ItemSearcher {
      * @param marketplace specified marketplace (ex: "CA" for Canada) to search in Amazon
      */
     private String keywordstext(String keywords, String marketplace) {
-
         return "https://amazon-price1.p.rapidapi.com/search?keywords=" + keywords.replace(" ", "%20") + "&marketplace=" + marketplace;
     }
 
@@ -148,16 +131,10 @@ public class ItemSearcher {
         ArrayList<String> listImgs = new ArrayList<>();
         try {
             Document doc = Jsoup.connect(url).timeout(10000).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36").get();
-
             Elements productUrls = doc.select("h2.a-size-mini.a-spacing-none.a-color-base a");
-
-
             for (Element item : productUrls) {
-
                 String newUrl = "https://www.amazon.ca" + item.attr("href");
                 listUrls.add(newUrl);
-
-
             }
             int counter = 0;
             for (String itemUrl : listUrls) {
@@ -165,22 +142,15 @@ public class ItemSearcher {
                     break;
                 }
                 Item item = searchItemUrl(itemUrl, true);
-
                 if (item.getItemName() != "" && item.getItemPrice() != 0) {
                     itemList.add(item);
                     counter += 1;
                 }
-
             }
-
             return itemList;
-
         } catch (IOException e) {
             return new ArrayList<Item>();
-
         }
-
-
     }
 
     /**
@@ -189,7 +159,6 @@ public class ItemSearcher {
      * @param url url of amazon item
      */
     public Item searchItemUrl(String url, boolean searchByKeyword) throws IOException {
-
         try {
             // This line specifies window type and layout of amazon page based on  Window Version and browser for webscraping
             Document doc = Jsoup.connect(url).timeout(10000).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36").get();
@@ -209,47 +178,29 @@ public class ItemSearcher {
             String imgUrl = "";
             int countRating = 0;
             double starRating = 0;
-
-
             String sellingPriceStr = price.text().replace(",", "").substring(1);
 
             if (!sellingPriceStr.matches(".*[a-zA-Z]+.*")) {
                 sellingPrice = Double.parseDouble(sellingPriceStr);
             }
-
-
             if (htmlCountRating != null) {
                 countRating = Integer.parseInt(htmlCountRating.text().replace(",", "").split(" ")[0]);
             }
-
             if (htmlStarRating != null) {
                 starRating = Double.parseDouble(htmlStarRating.text().split(" ")[0]);
             }
-
             if (htmlImgUrl != null) {
                 imgUrl = htmlImgUrl.attr("src");
             }
-
             if (htmlName != null) {
                 name = htmlName.text();
             }
-
-
             if (htmlDescription != null) {
                 description = htmlDescription.text();
-
             }
-
-
             return new Item(name, sellingPrice, sellingPrice, url, description, new String[]{}, countRating, starRating, imgUrl);
-
-
         } catch (IOException e) {
             return new Item("", 0, 0, "", "", new String[]{}, 0, 0, "");
         }
-
-
     }
-
-
 }
