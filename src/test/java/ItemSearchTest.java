5f1a451b54523d7a5dbae9d9d5c416a3078589b3
Kevin Le <kevinle6231@gmail.com>
2022-11-30 12:47:53 -0500

    some testing will pass and fail overtime since price of products on amazon can change

--- src/test/java/ItemSearchTest.java
+++ src/test/java/ItemSearchTest.java
@@ -34,17 +34,17 @@ public class ItemSearchTest {
     /**
      * Testing search item by url feature and if it is scraping the correct value of price
      */
-    @Test
-    public void searchItemTestKittenPlushPrice() throws IOException, InterruptedException {
-        ItemSearcher itemSearcher = new ItemSearcher();
-
-        String kittenplush = "https://www.amazon.ca/Kitten-Plush-Stuffed-Animal-Pillow/dp/B088BWPFYZ/ref=sr_1_5?crid=16S7RSE5N25AA&keywords=uwu+cat&qid=1669082008&qu=eyJxc2MiOiIyLjI1IiwicXNhIjoiMC4wMCIsInFzcCI6IjAuMDAifQ%3D%3D&sprefix=uwu+cat%2Caps%2C69&sr=8-5";
-        double expectedPrice = 28.99;
-        Item kittenplushitem = itemSearcher.searchItemUrl(kittenplush, false);
-
-        Assertions.assertEquals(kittenplushitem.getProductPrice(), expectedPrice);
-
-    }
+//    @Test
+//    public void searchItemTestKittenPlushPrice() throws IOException, InterruptedException {
+//        ItemSearcher itemSearcher = new ItemSearcher();
+//
+//        String kittenplush = "https://www.amazon.ca/Kitten-Plush-Stuffed-Animal-Pillow/dp/B088BWPFYZ/ref=sr_1_5?crid=16S7RSE5N25AA&keywords=uwu+cat&qid=1669082008&qu=eyJxc2MiOiIyLjI1IiwicXNhIjoiMC4wMCIsInFzcCI6IjAuMDAifQ%3D%3D&sprefix=uwu+cat%2Caps%2C69&sr=8-5";
+//        double expectedPrice = 28.99;
+//        Item kittenplushitem = itemSearcher.searchItemUrl(kittenplush, false);
+//
+//        Assertions.assertEquals(kittenplushitem.getProductPrice(), expectedPrice);
+//
+//    }
 
     /**
      * Testing search item by url feature and if it is scraping the correct value of name
@@ -63,16 +63,16 @@ public class ItemSearchTest {
     /**
      * Testing search item by url feature and if it is scraping the correct value of number of ratings for the product
      */
-    @Test
-    public void searchItemCarnivalCountRatingTest() throws IOException, InterruptedException {
-        ItemSearcher itemSearcher = new ItemSearcher();
-
-        String carnivalplush = "https://www.amazon.ca/BB-Funhouse-Stuffed-Carnival-3-14inch/dp/B09NRGTT1R/ref=sr_1_7?crid=1QCCDI9NB8WIQ&keywords=whale+plush&qid=1669082056&qu=eyJxc2MiOiI2LjAyIiwicXNhIjoiNS4xMiIsInFzcCI6IjQuNTQifQ%3D%3D&sprefix=whale+plushie%2Caps%2C95&sr=8-7";
-        Integer expectedCount = 7;
-        Item kittenplushitem = itemSearcher.searchItemUrl(carnivalplush, false);
-
-        Assertions.assertEquals(kittenplushitem.getReviewCount(), expectedCount);
-
-    }
+//    @Test
+//    public void searchItemCarnivalCountRatingTest() throws IOException, InterruptedException {
+//        ItemSearcher itemSearcher = new ItemSearcher();
+//
+//        String carnivalplush = "https://www.amazon.ca/BB-Funhouse-Stuffed-Carnival-3-14inch/dp/B09NRGTT1R/ref=sr_1_7?crid=1QCCDI9NB8WIQ&keywords=whale+plush&qid=1669082056&qu=eyJxc2MiOiI2LjAyIiwicXNhIjoiNS4xMiIsInFzcCI6IjQuNTQifQ%3D%3D&sprefix=whale+plushie%2Caps%2C95&sr=8-7";
+//        Integer expectedCount = 7;
+//        Item kittenplushitem = itemSearcher.searchItemUrl(carnivalplush, false);
+//
+//        Assertions.assertEquals(kittenplushitem.getReviewCount(), expectedCount);
+//
+//    }
 }
 
