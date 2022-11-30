670909f3a2beb47a403d925f969dd21e52c5e007
linajin03 <53534844+linajin03@users.noreply.github.com>
2022-11-30 10:41:05 -0500

    Trying to fix merge errors and create full functionality

--- src/main/java/GUI/WelcomePage.java
+++ src/main/java/GUI/WelcomePage.java
@@ -1,32 +1,34 @@
 package GUI;
 
+import Controller.UserRegisterController;
 import GUI.Listeners.WelcomePageActionListenerSwitchPanels;
-
+import UseCases.LoginAction;
 import javax.swing.*;
 import java.awt.*;
 
 public class WelcomePage extends JFrame{
+    UserRegisterController userRegisterController;
     private JPanel mainPanel;
     // header
-//    private JLabel titleLabel;
-//
-//    // Login Page
-//    private JLabel usernameLabel;
-//    private JTextField usernameField;
-//    private JLabel passwordLabel;
-//    private JPasswordField passwordField;
-//    private JButton loginButton;
-//
-//    // Sign Up Page
-//    private JTextField createUsernameField;
-//    private JPasswordField createPasswordField;
-//    private JLabel confirmPasswordLabel;
-//    private JPasswordField confirmPasswordField;
-//    private JButton signupButton;
-//
-//    // footer
-//    private JButton gotoLoginPanelButton;
-//    private JButton gotoSignupPanelButton;
+    private JLabel titleLabel;
+
+    // Login Page
+    private JLabel usernameLabel;
+    private JTextField usernameField;
+    private JLabel passwordLabel;
+    private JPasswordField passwordField;
+    private JButton loginButton;
+
+    // Sign Up Page
+    private JTextField createUsernameField;
+    private JPasswordField createPasswordField;
+    private JLabel confirmPasswordLabel;
+    private JPasswordField confirmPasswordField;
+    private JButton signupButton;
+
+    // footer
+    private JButton gotoLoginPanelButton;
+    private JButton gotoSignupPanelButton;
     private Color color1 = new Color(194, 234, 186);
     private Color color2 = new Color(106, 189, 154);
     private Font buttonFont = new Font("Sans Serif", Font.PLAIN, 12);
@@ -35,8 +37,9 @@ public class WelcomePage extends JFrame{
         return mainPanel;
     }
 
-    public WelcomePage() {
+    public WelcomePage(UserRegisterController controller) {
         super("Login Page");
+        this.userRegisterController = controller;
         setLayout(null);
         setSize(360, 640);
         setResizable(false);
@@ -71,13 +74,13 @@ public class WelcomePage extends JFrame{
         switchToLoginButton.addActionListener(wpal.getLoginActionListener());
         switchToSignupButton.addActionListener(wpal.getSignupActionListener());
 
-//        // footer panel
-//        footerPanel = new JPanel(new FlowLayout());
-//        gotoLoginPanelButton = new JButton("Login");
-//        gotoSignupPanelButton = new JButton("Sign Up");
-//        footerPanel.add(gotoLoginPanelButton);
-//        footerPanel.add(gotoSignupPanelButton);
-//        mainPanel.add(footerPanel, BorderLayout.SOUTH);
+        // footer panel
+        JPanel footerPanel = new JPanel(new FlowLayout());
+        gotoLoginPanelButton = new JButton("Login");
+        gotoSignupPanelButton = new JButton("Sign Up");
+        footerPanel.add(gotoLoginPanelButton);
+        footerPanel.add(gotoSignupPanelButton);
+        mainPanel.add(footerPanel, BorderLayout.SOUTH);
 
 
 //
@@ -96,20 +99,56 @@ public class WelcomePage extends JFrame{
 //                // TODO: Add login logic
 //            }
 //        });
+        loginButton.addActionListener(e -> {
+            HomePage homePage = new HomePage();
+
+            LoginAction login = new LoginAction(usernameField.getText(),
+                    String.valueOf(passwordField.getPassword()));
+            if (login.checkUsername() == false | login.checkUserMatchesPassword() == false){
+                JOptionPane.showMessageDialog(loginButton,
+                        "User and/or password does not match the records in our system. Please try again.");
+            }
+            else if (login.checkUserMatchesPassword() == true) {
+                homePage.setContentPane(homePage.getMainPanel());
+                homePage.setVisible(true);
+                homePage.setLocationRelativeTo(null);
+                homePage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                dispose();
+            }
+        });
 //
-//        signupButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                GUI.ListOfWishlistsPage listOfWishlists = new GUI.ListOfWishlistsPage();
-//                listOfWishlists.setContentPane(listOfWishlists.getMainPanel());
-//                listOfWishlists.setVisible(true);
-//                listOfWishlists.setLocationRelativeTo(null);
-//                listOfWishlists.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//                dispose();
+//        signupButton.addActionListener(e -> {
+//            ListOfWishlistsPage listOfWishlists = new ListOfWishlistsPage();
+//            listOfWishlists.setContentPane(listOfWishlists.getMainPanel());
+//            listOfWishlists.setVisible(true);
+//            listOfWishlists.setLocationRelativeTo(null);
+//            listOfWishlists.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//            dispose();
 //
-//                // TODO: Add sign up logic
+//            try{
+//                this.userRegisterController.create(createUsernameField.getText(), String.valueOf(createPasswordField.getPassword()));
+//                JOptionPane.showMessageDialog(signupButton, "User created.");
+//
+//            } catch(Exception message){
+//                JOptionPane.showMessageDialog(signupButton, message.getMessage());
 //            }
 //        });
+        signupButton.addActionListener(e -> {
+            HomePage homePage = new HomePage();
+            homePage.setContentPane(homePage.getMainPanel());
+            homePage.setVisible(true);
+            homePage.setLocationRelativeTo(null);
+            homePage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            dispose();
+
+            try{
+                this.userRegisterController.create(createUsernameField.getText(), String.valueOf(createPasswordField.getPassword()));
+                JOptionPane.showMessageDialog(signupButton, "User created.");
+
+            } catch(Exception message){
+                JOptionPane.showMessageDialog(signupButton, message.getMessage());
+            }
+        });
 
     }
 
