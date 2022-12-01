d64682a410351ecac44e846010e2a286770661e1
linajin03 <53534844+linajin03@users.noreply.github.com>
2022-12-01 14:49:13 -0500

    I have integrated login and user register functionality to WelcomePage and refactored UserRegister classes into a package in UseCases, Controller, GUI, and ExternalInterface to implement a Clean Architecture structure. You should now be able to log in with user "Herman1" and "Password". Though I think username is case sensitive.

--- src/main/java/GUI/WelcomePage.java
+++ src/main/java/GUI/WelcomePage.java
@@ -2,7 +2,6 @@ package GUI;
 
 import Controller.UserRegisterController;
 import GUI.Listeners.WelcomePageActionListenerSwitchPanels;
-import UseCases.LoginAction;
 import javax.swing.*;
 import java.awt.*;
 
@@ -47,7 +46,7 @@ public class WelcomePage extends JFrame{
         JLabel titleLabel = new JLabel("Starlight Wishes");
         titleLabel.setFont(titleFont);
         titleLabel.setForeground(Color.WHITE);
-        titleLabel.setBounds(103,13,154,24);
+        titleLabel.setBounds(103, 13, 154, 24);
 
         mainPanel = new JPanel(null);
         mainPanel.setBounds(0, 0, 360, 640);
@@ -57,16 +56,16 @@ public class WelcomePage extends JFrame{
         JPanel loginPanel = new LoginPanel(this);
         mainPanel.add(loginPanel);
 
-        JPanel signupPanel = new SignupPanel(this);
+        JPanel signupPanel = new SignupPanel(this, userRegisterController);
 
-        CustomJButton switchToLoginButton = new CustomJButton("Login Page",0,0,
+        CustomJButton switchToLoginButton = new CustomJButton("Login Page", 0, 0,
                 Color.WHITE, Color.BLACK,
                 buttonFont);
-        switchToLoginButton.setBounds(30,550,130,15);
-        CustomJButton switchToSignupButton = new CustomJButton("Sign Up Page",0,0,
+        switchToLoginButton.setBounds(30, 550, 130, 15);
+        CustomJButton switchToSignupButton = new CustomJButton("Sign Up Page", 0, 0,
                 Color.WHITE, Color.BLACK,
                 buttonFont);
-        switchToSignupButton.setBounds(190,550,130,15);
+        switchToSignupButton.setBounds(190, 550, 130, 15);
         mainPanel.add(switchToLoginButton);
         mainPanel.add(switchToSignupButton);
 
@@ -81,75 +80,5 @@ public class WelcomePage extends JFrame{
         footerPanel.add(gotoLoginPanelButton);
         footerPanel.add(gotoSignupPanelButton);
         mainPanel.add(footerPanel, BorderLayout.SOUTH);
-
-
-//
-//        // Button Logic
-//
-//        loginButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                GUI.ListOfWishlistsPage listOfWishlists = new GUI.ListOfWishlistsPage();
-//                listOfWishlists.setContentPane(listOfWishlists.getMainPanel());
-//                listOfWishlists.setVisible(true);
-//                listOfWishlists.setLocationRelativeTo(null);
-//                listOfWishlists.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//                dispose();
-//
-//                // TODO: Add login logic
-//            }
-//        });
-        loginButton.addActionListener(e -> {
-            HomePage homePage = new HomePage();
-
-            LoginAction login = new LoginAction(usernameField.getText(),
-                    String.valueOf(passwordField.getPassword()));
-            if (login.checkUsername() == false | login.checkUserMatchesPassword() == false){
-                JOptionPane.showMessageDialog(loginButton,
-                        "User and/or password does not match the records in our system. Please try again.");
-            }
-            else if (login.checkUserMatchesPassword() == true) {
-                homePage.setContentPane(homePage.getMainPanel());
-                homePage.setVisible(true);
-                homePage.setLocationRelativeTo(null);
-                homePage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                dispose();
-            }
-        });
-//
-//        signupButton.addActionListener(e -> {
-//            ListOfWishlistsPage listOfWishlists = new ListOfWishlistsPage();
-//            listOfWishlists.setContentPane(listOfWishlists.getMainPanel());
-//            listOfWishlists.setVisible(true);
-//            listOfWishlists.setLocationRelativeTo(null);
-//            listOfWishlists.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//            dispose();
-//
-//            try{
-//                this.userRegisterController.create(createUsernameField.getText(), String.valueOf(createPasswordField.getPassword()));
-//                JOptionPane.showMessageDialog(signupButton, "User created.");
-//
-//            } catch(Exception message){
-//                JOptionPane.showMessageDialog(signupButton, message.getMessage());
-//            }
-//        });
-        signupButton.addActionListener(e -> {
-            HomePage homePage = new HomePage();
-            homePage.setContentPane(homePage.getMainPanel());
-            homePage.setVisible(true);
-            homePage.setLocationRelativeTo(null);
-            homePage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            dispose();
-
-            try{
-                this.userRegisterController.create(createUsernameField.getText(), String.valueOf(createPasswordField.getPassword()));
-                JOptionPane.showMessageDialog(signupButton, "User created.");
-
-            } catch(Exception message){
-                JOptionPane.showMessageDialog(signupButton, message.getMessage());
-            }
-        });
-
     }
-
 }
