022d7873c88e2bac604fc871f1752448f7948fb5
Kevin Le <kevinle6231@gmail.com>
2022-12-05 12:57:47 -0500

    implementing ListofProductLists interface to abide with clean arch SOLID inversion dependency princicple

--- src/main/java/Entities/ListofProductLists.java
+++ src/main/java/Entities/ListofProductLists.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 /**
- * An interface that specifies the functionalities expected from a product
+ * An interface that specifies the functionalities expected from a list of product lists
  */
 public interface ListofProductLists {
     ArrayList<ProductList> getListOfWishlist();
