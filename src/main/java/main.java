7dd79c908be92d71412aa32e00be473ffd431d77
Kevin Le <kevinle6231@gmail.com>
2022-12-07 23:37:23 -0500

    fixed warnings

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,23 +1,14 @@
 import Controller.UserRegisterController;
-import DataBase.DataBaseController;
-import Entities.ListOfProductLists;
-import Entities.ListOfWishlists;
-import Entities.User;
-import Entities.Wishlist;
 import GUI.WelcomePage;
 import UseCases.UserRegister.*;
 
 import javax.swing.*;
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
 public class main {
     /**
      * main
      */
 
-    public main(String[] args) throws ParseException, IOException, org.json.simple.parser.ParseException {
+    public static void main(String[] args) {
         // Schedule a job for the event-dispatching thread:
         // creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(main::createAndShowGUI);
